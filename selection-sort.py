56513126e429e05442ffd06c4520a709ad4a4c7f
joaol-dev <joaolucasbittencourt01@gmail.com>
2025-05-27 22:21:15 -0300

    Create selection-sort.py

---
+++ selection-sort.py
@@ -0,0 +1,18 @@
+
+
+
+def selection_sort(lista):
+    n = len(lista)
+    for p in range(n - 1):
+        min_indice = p
+        for i in range(p, n):
+            if lista[i] < lista[min_indice]:
+                min_indice = i
+        if lista[p] > lista[min_indice]:
+            aux = lista[p]
+            lista[p] = lista[min_indice]
+            lista[min_indice] = aux
+
+lista = [5, 8, 2, 9, 3, 10]
+selection_sort(lista)
+print(lista)
