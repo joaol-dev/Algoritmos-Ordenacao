5848a93ceb9a558a75a0f8989c3bb704a656f191
joaol-dev <joaolucasbittencourt01@gmail.com>
2025-05-29 14:28:15 -0300

    1.1 bubble

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+lista = [4, 9, 2, 1, 7, 8]
+
+def bubble_sort(lista):
+    n = len(lista)
+    for j in range(n - 1):
+        for i in range(n - 1):
+            if lista[i] > lista[i + 1]:
+                lista[i], lista[i + 1] = lista[i + 1], lista[i] # 
